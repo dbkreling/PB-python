b5f485b755b3c47ff56950baa82c5e4e4e644b5a
Daniel Kreling <dbkreling@br.ibm.com>
2017-03-24 13:48:01 -0300

    PB-python: changing names of test files
    Changing names of the test files will be necessary to run unittest modules
    in order to collect the output of the tests to be consumed by the back end
    layer. Unit tests use the regular expression pattern "test*" to collect the
    test files.
    
    Signed-off-by: Daniel Kreling <dbkreling@br.ibm.com>

--- tests/test_fail_0.py
+++ tests/test_fail_0.py
@@ -5,11 +5,15 @@ import unittest
 
 
 class MyTest1(unittest.TestCase):
-    def test_fail_1(self):
+    def test_fail_0(self):
         var_1 = 1
         var_2 = 2
-        return self.assertTrue(var_1, var_2)
+        return self.assertEquals(var_1, var_2)
+
+def main():
+    unittest.main()
+
 
 
 if __name__ == '__main__':
-    unittest.main()
+    main()
